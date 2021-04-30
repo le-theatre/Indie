55c53774ba1e014248b0f46475dc70151b531f7b
Romain <romain.minguet@epitech.eu>
2021-04-30 16:34:08 +0200

    feat: project sketch

---
+++ ecs/IComponent.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** IComponent
+*/
+
+#ifndef ICOMPONENT_HPP_
+#define ICOMPONENT_HPP_
+
+namespace ecs {
+
+class IComponent {
+    public:
+        IComponent();
+        ~IComponent();
+
+    protected:
+    private:
+};
+
+}
+
+#endif /* !ICOMPONENT_HPP_ */
---
+++ ecs/IEntity.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** Indie
+** File description:
+** IEntity
+*/
+
+#ifndef IENTITY_HPP_
+#define IENTITY_HPP_
+
+class IEntity {
+    public:
+        IEntity();
+        ~IEntity();
+
+    protected:
+    private:
+};
+
+#endif /* !IENTITY_HPP_ */
