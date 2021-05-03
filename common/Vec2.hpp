316427ff642caefb60ad3ad5d772124024751494
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2021-05-03 14:50:01 +0200

    feat(common): 3d and 2d vector

---
+++ common/Vec2.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** Vec2
+*/
+
+#pragma once
+
+template <typename TValue>
+class Vec2
+{
+    Vec2(TValue x = 0, TValue y = 0) : x(x), y(y)
+    {
+    }
+
+    Vec2 &operator+=(const Vec2 &other)
+    {
+        x += other.x;
+        y += other.y;
+    }
+
+    Vec2 &operator-=(const Vec2 &other)
+    {
+        x -= other.x;
+        y -= other.y;
+    }
+
+    Vec2 operator+(const Vec2 &other)
+    {
+        return Vec2(other.x + x, other.y + y);
+    }
+
+    Vec2 operator-(const Vec2 &other)
+    {
+        return Vec2(other.x - x, other.y - y);
+    }
+
+    bool operator==(const Vec2 &other) const
+    {
+        return (x == other.x && y == other.y);
+    }
+
+    void set(TValue x, TValue y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    TValue x;
+    TValue y;
+};
+
+using Vec2d = Vec2<double>;
+using Vec2f = Vec2<float>;
+using Vec2i = Vec2<int>;
+using Vec2u = Vec2<unsigned int>;
--- common/Vec3.hpp
+++ common/Vec3.hpp
@@ -1,17 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** Vec3
+*/
+
 #pragma once
 
-namespace ecs {
-
 template <typename TValue>
-class Vec3 {
-    TValue x;
-    TValue y;
-    TValue z;
-
-    Vec3(TValue _x = 0, TValue _y = 0, TValue _z = 0) :
-        x(_x), y(_y), z(_z)
+class Vec3
+{
+    Vec3(TValue x = 0, TValue y = 0, TValue z = 0) : x(x), y(y), z(z)
     {
-
     }
 
     Vec3 &operator+=(const Vec3 &other)
@@ -28,17 +28,34 @@ class Vec3 {
         z -= other.z;
     }
 
+    Vec3 operator+(const Vec3 &other)
+    {
+        return Vec3(other.x + x, other.y + y, other.z + z);
+    }
+
+    Vec3 operator-(const Vec3 &other)
+    {
+        return Vec3(other.x - x, other.y - y, other.z - z);
+    }
+
     bool operator==(const Vec3 &other) const
     {
         return (x == other.x && y == other.y && z == other.z);
     }
 
+    void set(TValue x, TValue y, TValue z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
 
+    TValue x;
+    TValue y;
+    TValue z;
 };
 
 using Vec3d = Vec3<double>;
 using Vec3f = Vec3<float>;
 using Vec3i = Vec3<int>;
 using Vec3u = Vec3<unsigned int>;
-
-}
