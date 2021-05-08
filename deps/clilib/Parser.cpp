9b2ed8971e1e433ee06faa00455ffc428d899a21
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2021-05-08 10:28:54 +0200

    feat(deps): clilib implementation

---
+++ deps/clilib/Parser.cpp
@@ -0,0 +1,113 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** Parser
+*/
+
+#include <iostream>
+#include "clilib/Parser.hpp"
+
+namespace clilib {
+
+Parser::Parser(int argc, char const **argv) :
+    m_argc(argc),
+    m_argv(argv),
+    m_flags(),
+    m_options(),
+    m_container(nullptr)
+{
+}
+
+void Parser::setNonOptionsStore(std::vector<std::string> &container)
+{
+    m_container = &container;
+}
+
+void Parser::addOption(const std::string &longOption,
+    const std::string &shortOption,
+    OptionHandler &&handler)
+{
+    m_options.emplace_back(OptionData{longOption, shortOption, handler, false});
+}
+
+void Parser::addFlag(const std::string &longOption,
+    const std::string &shortOption,
+    FlagHandler &&handler)
+{
+    m_flags.emplace_back(FlagData{longOption, shortOption, handler, false});
+}
+
+void Parser::addRequiredOption(const std::string &longOption,
+    const std::string &shortOption,
+    OptionHandler &&handler)
+{
+    m_options.emplace_back(OptionData{longOption, shortOption, handler, true});
+}
+
+void Parser::addRequiredFlag(const std::string &longOption,
+    const std::string &shortOption,
+    FlagHandler &&handler)
+{
+    m_flags.emplace_back(FlagData{longOption, shortOption, handler, true});
+}
+
+bool Parser::parse() noexcept
+{
+    try {
+        for (std::size_t i = 0; i < m_argc; ++i) {
+            if (i + 1 < m_argc && searchMatchingOption(m_argv[i], m_argv[i + 1])) {
+                continue;
+            } else if (!searchMatchingFlag(m_argv[i]) && m_container != nullptr) {
+                m_container->emplace_back(m_argv[i]);
+            }
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return false;
+    }
+    return checkRequired();
+}
+
+bool Parser::searchMatchingOption(const std::string &value, const std::string &nextValue)
+{
+    for (auto &option : m_options) {
+        if (value == option.shortOption || value == option.longOption) {
+            if (!option.handler(nextValue)) {
+                throw std::exception();
+            }
+            option.required = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Parser::searchMatchingFlag(const std::string &value)
+{
+    for (auto &flag : m_flags) {
+        if (value == flag.shortOption || value == flag.longOption) {
+            flag.handler();
+            flag.required = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Parser::checkRequired()
+{
+    for (const auto& option : m_options) {
+        if (option.required) {
+            return false;
+        }
+    }
+    for (const auto& flag : m_flags) {
+        if (flag.required) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
---
+++ deps/clilib/Parser.hpp
@@ -0,0 +1,81 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** Parser
+*/
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include <tuple>
+#include <vector>
+
+namespace clilib {
+
+class Parser {
+public:
+    using FlagHandler = std::function<void()>;
+    using OptionHandler = std::function<bool(const std::string &)>;
+
+    Parser(int argc, char const **argv);
+
+    /// All arguments that were not identified as flags will be pushed into
+    /// container.
+    void setNonOptionsStore(std::vector<std::string> &container);
+
+    /// Register an command line option. Upon encoutering it, handler will be
+    /// called with the next argument.
+    /// If one of the handler for a given option returns false, a call to
+    /// parse will fail.
+    void addOption(const std::string &longOption,
+        const std::string &shortOption,
+        OptionHandler &&handler);
+
+    /// Register an command line flag. Upon encoutering it, handler will be
+    /// called.
+    void addFlag(const std::string &longOption,
+        const std::string &shortOption,
+        FlagHandler &&handler);
+
+    /// Same as addOption though a call to parse will fail if this option is
+    /// never encountered.
+    void addRequiredOption(const std::string &longOption,
+        const std::string &shortOption,
+        OptionHandler &&handler);
+
+    /// Same as addFlag though a call to parse will fail if this flag is never
+    /// encountered.
+    void addRequiredFlag(const std::string &longOption,
+        const std::string &shortOption,
+        FlagHandler &&handler);
+
+    /// Parse will return false if a required option or flag is missing, if one
+    /// of the option handlers returned false or if an exception was thrown
+    /// inside of a handler.
+    bool parse() noexcept;
+
+private:
+    template <typename THandler> struct Data {
+        std::string shortOption;
+        std::string longOption;
+        THandler handler;
+        bool required;
+    };
+
+    using FlagData = Data<FlagHandler>;
+    using OptionData = Data<OptionHandler>;
+
+    bool searchMatchingOption(const std::string &value, const std::string &nextValue);
+    bool searchMatchingFlag(const std::string &value);
+    bool checkRequired();
+
+    int m_argc;
+    char const **m_argv;
+    std::vector<FlagData> m_flags;
+    std::vector<OptionData> m_options;
+    std::vector<std::string> *m_container;
+};
+
+}
