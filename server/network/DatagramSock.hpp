5ed86b5c1d813de6a6d19a64f5f799164b7cfbe3
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2021-05-09 08:01:20 +0200

    build: cmake lists

---
+++ server/network/DatagramSock.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** Datagram
+*/
+
+#pragma once
+
+namespace server::network {
+
+class DatagramSock {
+public:
+
+private:
+
+};
+
+}
---
+++ server/network/StreamSock.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** indie
+** File description:
+** StreamSock
+*/
+
+#pragma once
+
+namespace server::network {
+
+}
